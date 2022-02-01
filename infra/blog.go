954c3689fabf99e277d6d72af977108197365c10
b9p31049 <b9p31049@shonan.bunkyo.ac.jp>
2022-02-01 20:34:27 +0900

    infrastructure impl

---
+++ infra/blog.go
@@ -0,0 +1,55 @@
+package infra
+
+import (
+	"engineer-jobhunting-api/domain/model"
+	"engineer-jobhunting-api/domain/repository"
+	"gorm.io/gorm"
+)
+
+// BlogRepository : blog repository の 構造体
+type BlogRepository struct {
+	Conn *gorm.DB
+}
+
+// NewBlogRepository : blog repositoryのコンストラクタ
+func NewBlogRepository(conn *gorm.DB) repository.BlogRepository {
+	return &BlogRepository{Conn: conn}
+}
+
+// Create blogの保存
+func (br *BlogRepository) Create(blog *model.Blog) (*model.Blog, error) {
+	if err := br.Conn.Create(&blog).Error; err != nil {
+		return nil, err
+	}
+
+	return blog, nil
+}
+
+// FindByID : blogをIDで取得
+func (br *BlogRepository) FindByID(id int) (*model.Blog, error) {
+	blog := &model.Blog{ID: id}
+
+	if err := br.Conn.First(&blog).Error; err != nil {
+		return nil, err
+	}
+
+	return blog, nil
+}
+
+// Update : blogの更新
+func (br *BlogRepository) Update(blog *model.Blog) (*model.Blog, error) {
+	if err := br.Conn.Model(&blog).Updates(&blog).Error; err != nil {
+		return nil, err
+	}
+
+	return blog, nil
+}
+
+// Delete : blogの削除
+func (br *BlogRepository) Delete(blog *model.Blog) error {
+	if err := br.Conn.Delete(&blog).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
